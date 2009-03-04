29c961ba4008c1af41c8da60454b489bb6248ef1
Matt Wayment <matt@starchey.com>
2009-03-04 09:18:06 +0000

    - added new model

--- lib/db.class.php
+++ lib/db.class.php
@@ -1,286 +1,54 @@
 <?php
 
-/**
- * DB Wrapper Class with custom exception handling
- * and query methods
- */
-class DB extends mysqli {
-	// Result Object from query
-	private $result;
-	
-	// Constants for setting the result type array
-	const ASSOC = 1;
-	const NUM = 2;
-	const BOTH = 3;
-	
-	/**
-	 * DB class constructor
-	 * @throws ConnectException
-	 */
+class DB extends PDO {
+
 	function __construct() {
-		//require('connect.inc');
 		$settings = Factory::get_config()->get_database_info();
-	  	parent::__construct(
-	  	$settings['host'],
-	  	$settings['username'],
-	  	$settings['password'],
-	  	$settings['database']);
-
-       	if (mysqli_connect_errno($this)) {
-       		throw new ConnectException(mysqli_connect_error($this), mysqli_connect_errno($this));
-       	}
-	}
-	
-	/**
-	 * DB class destructor
-	 * Closes the DB Connection
-	 */
-	public function __destruct() {
-    	if (!mysqli_connect_errno($this)) {
-        	$this->close();
-       	}
-   	}
-
-   	/**
-   	 * Overrides mysqli->query
-   	 * Runs the SQL query and returns the result
-   	 * @param string $query SQL query string
-   	 * @return Result Object
-   	 */
-	function query($query) {
-		$this->result = parent::query($query);
-    	if(mysqli_error($this)){
-    		throw new QueryException(mysqli_error($this), mysqli_errno($this));
-    	}
-    
-    	return $this->result;
-  	}
-
-  	/**
-  	 * Runs the SQL query and returns the last element
-  	 * from the first row of the result set.  Useful with queries
-  	 * that return a single element (ex. 'Select now()')
-  	 * @param string $query SQL query string
-  	 * @return Single Element from result 
-  	 */
-  	function getSingleElement($query) {
-  		$row = $this->query($query)->fetch_row();
-  		
-  		return (is_array($row) ? array_pop($row) : $row);
-	}
-
-	/**
-	 * Runs the SQL query and returns the first row
-	 * of the result set.  Useful with queries that
-	 * return a single row (ex. 'Select user_id, user_name
-	 * from users where user_id = 1')
-	 * @param string $query SQL query string
-	 * @param const $result_type (optional) sets the array 
-	 * result type
-	 * @return array First row from result set  
-	 */
-	function getSingleRow($query, $result_type = DB::BOTH) {
-		return $this->query($query)->fetch_array($result_type);
-	}
-	
-	/**
-	 * Runs the SQL query and returns the specified row
-	 * of the result set.
-	 * @param string $query SQL query string
-	 * @param int $row_num Row index of result set to return
-	 * @param const $result_type (optional) sets the array
-	 * result type
-	 * @return array Specified row from result set
-	 */
-	function getSpecificRow($query, $row_num, $result_type = DB::BOTH) {
-		$this->result = $this->query($query);
-		$this->result->data_seek($row_num);
-		$row = $this->result->fetch_array($result_type);
-		return $row;
-	}
-	
-	/**
-	 * Runs the SQL query and returns a multi-dimensional array
-	 * populated with the result set.
-	 * @param string $query SQL query string
-	 * @param const $result_type (optional) sets the array
-	 * result type
-	 * @return array Array of Results
-	 */
-	function getResultsArray($query, $result_type = DB::BOTH) {
-		$this->result = $this->query($query);
 		
-		$rows = $this->resultToArray($result_type);
-
-        $this->result->free_result();
-
-        return $rows;
+		parent::__construct('mysql:host='.
+		  	$settings['host'].';dbname='.
+		  	$settings['database'],
+		  	$settings['username'],
+		  	$settings['password']
+	  	);
+	  	
+	  	$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	
-	/**
-	 * Converts the Result Set Object into an array
-	 * @param const $result_type (optional) sets the array
-	 * result type
-	 * @return array Array of Results
-	 */
-	private function resultToArray($result_type = DB::BOTH) {
-		$rows = array();
-        while($row = $this->result->fetch_array($result_type)) {
-            $rows[] = $row;
-        }
-        
-        return $rows;
-	}
-	
-	/**
-	 * Overrides mysqli->multi_query
-	 * Executes multiple SQL queries, must use
-	 * mysqli_use_result() or mysqli_store_result()
-	 * to get results.
-	 * @param string $query SQL query strings (semicolon delimited)
-	 * @return bool False if first query fails
-	 */
-	function multi_query($query) {
-		$success = parent::multi_query($query);
-    	if(mysqli_error($this)){
-    		throw new QueryException(mysqli_error($this), mysqli_errno($this));
-    	}
-    	
-    	return $success;
-	}
-	
-	/**
-	 * Runs multiple SQL queries and returns a multi-dimensional array
-	 * of the results from each of the queries.
-	 * @param string $query SQL query strings (semicolon delimited)
-	 * @param const $result_type (optional) sets the array
-	 * result type
-	 * @return array Each result set is put into an integer-indexed
-	 * multi-dimensional array
-	 */
-	function getMultiQueryResults($query, $result_type = DB::BOTH) {
-		$multiresults = array();
+	function query($statement) {
+		$result = parent::query($statement);
 		
-		if ($this->multi_query($query)) {
-			do {
-				if ($this->result = $this->store_result()) {
-					$multiresults[] = $this->resultToArray($result_type);
-					$this->result->close();
-				}
-			} while ($this->next_result());
+		if ( !$result )
+		{
+			print_r( $this->errorInfo() );
 		}
-		
-		return $multiresults;
-	}
 
-	/**
-	 * Runs the SQL query and returns an object with properties
-	 * to match each column returned in the result set.
-	 * @param string $query SQL query string
-	 * @return object
-	 */
-	function getObject($query) {
-		$this->result = $this->query($query);
-		$object = $this->result->fetch_object();
-		
-		return $object;
-	}
-	
-	function getClassObject($query, &$data_object) {
-		$row = $this->getSingleRow($query, DB::ASSOC);
-		if (empty($row))
-			return FALSE;
-
-		$properties = array_keys($row);	
-		$class = new ReflectionObject($data_object);
-		
-		//$data_object = $this->buildObjectFromRow($data_object, $row, $class, $properties);
-		$this->buildObjectFromRow($data_object, $row, $class, $properties);
-		//return $data_object;
-		return TRUE;
-	}
-	
-	private function buildObjectFromRow(&$data_object, &$row, &$class, &$properties) {
-    	for ($i = 0; $i < count($properties); $i++) {
-    		$method = "set_" . $properties[$i];
-    		$property = $properties[$i];
-
-    		if ($class->hasProperty($property)) {
-  				$data_object->$method($row[$properties[$i]]);
-    		}
-    	}
-    
-    	//return (clone $data_object);
-	}
-	
-	function getClassObjectArray($query, $data_object) {
-		$this->result = $this->query($query);
-		$class = new ReflectionObject($data_object);
-
-		$objects = array();
-		$setup_properties = true;
-		$properties = array();
-		
-		while($row = $this->result->fetch_array(DB::ASSOC)) {
-			if ($setup_properties) {
-				$properties = array_keys($row);
-				$setup_properties = false;	
-			}
-			
-			$obj = clone($data_object);
-			$this->buildObjectFromRow($obj, $row, $class, $properties);
-			$objects[] = $obj;
-        }
-
-        return $objects;
-	}
-	
-	/**
-	 * Runs the SQL query and returns an array of objects 
-	 * with properties to match each column returned 
-	 * in the result set.
-	 * @param string $query SQL query string
-	 * @return array Array of Objects
-	 */
-	function getObjectArray($query) {
-		$this->result = $this->query($query);
-		$rows = array();
-
-        while($row = $this->result->fetch_object()) {
-            $rows[] = $row;
-        }
-
-        return $rows;
-	}
-	
-	/**
-	 * Overrides mysqli->prepare
-	 * Prepares a SQL statement for execution
-	 * @param string $query SQL query string
-	 * @return PreparedStatement object
-	 */
-	function prepare($query) {
-		$statement = parent::prepare($query);
-    	if(mysqli_error($this)){
-    		throw new StatementException(mysqli_error($this), mysqli_errno($this));
-    	}
-    	
-    	return $statement;
+		return $result;
   	}
+  	
+  	function getElement($statement) {
+  		$stmt = $this->prepare( $statement );
+  		$stmt->execute();
+  		$result = $stmt->fetchColumn();
+  		
+  		return $result;
+	}
+	
+	function getRow($statement, $fetch_style=PDO::FETCH_BOTH) {
+		$stmt = $this->prepare( $statement );
+  		$stmt->execute();
+  		$result = $stmt->fetch($fetch_style);
+  		
+  		return $result;
+	}
+	
+	function getObject($statement, $class) {
+		$stmt = $this->prepare( $statement );
+  		$stmt->execute();
+  		$result = $stmt->fetch(PDO::FETCH_INTO, $class);
+  		
+  		return $result;
+	}
 }
 
-/**
- * ConnectException class that is thrown for DB Connection errors
- */
-class ConnectException extends Exception {}
-
-/**
- * QueryException class that is thrown for SQL errors
- */
-class QueryException extends Exception {}
-
-/**
- * StatementException class that is thrown for preparing statement errors
- */
-class StatementException extends Exception {}
 ?>
---
+++ lib/db.driver.mysql.class.php
@@ -0,0 +1,338 @@
+<?php
+
+class DB_Driver_MySQL extends DB_Driver {
+	
+	public function __construct($table_name, $model_name, $model_properties) {
+        parent::__construct($table_name, $model_name, $model_properties);
+	}
+	
+	public function perform_create() {
+		$statement = "INSERT INTO " .
+		             $this->table_name . " " .
+		             "(" . $this->get_comma_separated_column_names(false, '', FL_CREATE) . ") " .
+		             "VALUES " .
+		             "(" . $this->get_comma_separated_question_marks(FL_CREATE) . ")";
+		             
+		$stmt = $this->db->prepare($statement);
+		$stmt->execute($this->get_column_values(true, FL_CREATE));
+		
+		// If multiple primary key, values should already have been set
+		
+		$return_value = null;
+		
+		if ($this->single_primary_key) {
+			$id = $this->db->lastInsertId();
+			$key_prop = $this->column_to_property($this->get_primary_key());
+			$this->$key_prop = $id;
+			$return_value = $id;
+		} else {
+			$keys = $this->get_primary_key();
+			$values = array();
+			foreach ($keys as $key) {
+				$values[] = $this->get($key);
+			}
+			$return_value = $values;
+		}
+		
+		return $return_value;
+	}
+	
+	public function perform_retrieve($id) {
+		$where = $this->get_where_statement();
+		
+		$primary_key = $this->get_primary_key(true);
+		if (is_array($primary_key)) {
+			$index = 0;
+			foreach ($primary_key as $key) {
+				if ($index > 0) {
+					$where .= " AND ";
+				}
+				$cur_id = $id[$index];
+				$where .= "$key=$cur_id";
+				$index++;
+			}
+		} else {
+			$where .= "$primary_key=$id";
+		}
+		
+		$statement = "SELECT " . 
+					 $this->get_select_statement(true, FL_RETRIEVE) . ' ' .
+					 "FROM {$this->get_from_statement()} WHERE $where";
+					 
+		//echo "RETRIEVE STATMENT: </PRE><code>$statement</code><PRE>\n";
+					 	 
+		$stmt = $this->db->prepare($statement);
+  		$stmt->execute();
+  		
+  		//echo "NUM ROWS FROM STATEMENT: " . $stmt->rowCount() . "\n";
+  		
+  		$rowFound = false;
+  		
+  		if ($stmt->rowCount() == 1) {
+  			$result = $stmt->fetch(PDO::FETCH_ASSOC); 		
+  			//print_r($result);
+  			// echo "\n\n";
+  			$this->populate($result, false);
+  			$rowFound = true;
+  		} else if ($stmt->rowCount() > 1) {
+  			$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  			// print_r($result);
+  			// echo "\n\n";
+  			$this->populate($result, true);
+  			$rowFound = true;
+  		}
+  		
+  		return $rowFound;
+	}
+	
+	public function perform_update() {
+		$statement = "UPDATE " .
+                     $this->table_name . " SET ";
+             
+        $first = true;
+        foreach($this->column_names as $column_name) {
+        	if (!in_array($column_name, $this->primary_key)) {
+        		
+        		if ($this->column_has_flag($column_name, FL_UPDATE)) {
+	        		if (!$first) {
+	        			$statement .= ", ";
+	        		}
+	        		$first = false;
+	        		$statement .= "$column_name=?";
+        		}
+        	}
+        }
+        
+        $statement .= " WHERE ";
+        
+       	$primary_key = $this->get_primary_key();
+       	if (is_array($primary_key)) {
+       		$first = true;
+       		foreach($primary_key as $key) {
+       			if (!$first) {
+       				$statement .= " AND ";
+       			}
+       			
+       			$first = false;
+       			$statement .= "$key=?";
+       		}
+       	} else {
+       		$statement .= "$primary_key=?";
+       	}
+        
+        $statement .= " LIMIT 1";
+
+		$stmt = $this->db->prepare($statement);
+		$stmt->execute($this->get_column_values(false, FL_UPDATE));
+
+		return true;
+	}
+	
+	public function perform_delete() {
+		$statement = "DELETE FROM " .
+		             $this->table_name . " WHERE ";
+        
+        $primary_key = $this->get_primary_key();
+        if (is_array($primary_key)) {
+            $first = true;
+            foreach ($primary_key as $key) {
+                if (!$first) {
+                    $statement .= " AND ";
+                }
+                
+                $first = false;
+                $statement .= "$key=?";
+            }
+        } else {
+            $statement .= "$primary_key=?";
+        }
+        
+        $statement .= " LIMIT 1";
+        
+        $values = array();
+        if (is_array($primary_key)) {
+            foreach ($primary_key as $key) {
+                $prop = $this->column_to_property($key);
+                $values[] = $this->$prop;
+            }
+        } else {
+            $prop = $this->column_to_property($primary_key);
+            $values[] = $this->$prop;
+        }
+		
+		$stmt = $this->db->prepare($statement);
+		$stmt->execute($values);
+		
+        return true;
+	}
+    
+    protected function perform_find() {
+		$found_rows = '';
+		if ($this->find_limit_count) {
+			$found_rows = 'SQL_CALC_FOUND_ROWS';
+		}
+		
+		$statement = "SELECT $found_rows " . 
+			         $this->get_select_statement(true, FL_FIND) . ' ' .
+			         "FROM {$this->get_from_statement()}";
+
+		if (!empty($this->find_where)) {
+			$statement .= " WHERE {$this->find_where} ";
+		}
+			         
+			         
+		if ($this->find_order_by) {
+			$first = true;
+			foreach($this->find_order_by as $order_by) {
+				if (!$first) {
+					$statement .= ', ';
+				} else {
+					$statement .= 'ORDER BY ';
+					$first = false;
+				}
+				
+				if ($this->relationships) {
+					$this->add_alias_to_column_names($order_by);
+				}
+				
+				$statement .= $order_by . ' ';
+			}
+		}
+		
+		if ($this->find_limit_count || $this->find_limit_offset) {
+			$statement .= 'LIMIT ';
+			
+			if ($this->find_limit_offset) {
+				$statement .= $this->find_limit_offset;
+			}
+			
+			if ($this->find_limit_count) {
+				if ($this->find_limit_offset) {
+					$statement .= ', ';
+				}
+				
+				$statement .= $this->find_limit_count;
+			}
+		}
+			         
+		//echo "FIND STATMENT: </PRE><code>$statement</code><PRE>\n";
+		// print_r( $where_vars );
+			         
+		$stmt = $this->db->prepare($statement);
+		$stmt->execute($this->find_where_vars);
+		
+		$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		
+		$objects = array();
+		$class_name = $this->derived_name;
+		foreach ($results as $result) {
+			$object = new $class_name();
+			$object->db_driver()->set_alias($this->get_alias());
+			$object->db_driver()->populate($result, false);
+			$objects[] = $object;
+		}
+
+		return $objects;
+	}
+    
+    protected function get_value_equals_statement($value, $and) {
+        $statement = "$value = ?";
+        
+        if ($and) {
+            $statement = "AND " . $statement;
+        }
+        
+        return $statement;
+    }
+    
+    protected function get_and_operator() {
+        return 'AND';
+    }
+    
+    protected function get_equals_operator() {
+        return '=';
+    }
+    
+    protected function get_alias_prefix() {
+        return $this->get_alias() . '.';
+    }
+    
+    protected function get_aliased_column_name($column_name) {
+        return $this->get_alias() . '.' . $column_name;
+    }
+    
+    protected function get_specific_aliased_column_name($column_name, $alias) {
+        return $alias . '.' . $column_name;
+    }
+    
+    protected function get_aliased_column_as_aliased($parent, $column_name) {
+        $value = "$parent" . $this->get_alias() . '.' . $column_name . " AS '" . 
+                 "$parent" . $this->get_alias() . '.' . $column_name . "'";
+                 
+        return $value;
+    }
+    
+    protected function get_from_table_statement($parent, $table_name, $alias) {
+        $from = ',' . $table_name . " {$parent}_{$alias}";
+        return $from;
+    }
+    
+    protected function get_table_name_with_alias() {
+        $from = $this->get_table_name();
+        $alias = $this->get_alias();
+        if (!empty($alias)) {
+            $from .= " $alias";
+        }
+        return $from;
+    }
+
+	public function get_found_rows_count() {
+		$stmt = $this->db->query( 'SELECT FOUND_ROWS()' );
+		$result = $stmt->fetch(PDO::FETCH_NUM);
+		return $result[0];
+	}
+	
+	protected function process_where_clause(&$clause, &$where_vars) {
+		// Find any likes, need to add % to where_vars
+		$offset = 0;
+		$length = 0;
+		$var_index = 0;
+		do {
+			$pos = strpos($clause, '?', $offset);
+			
+			if ($pos !== false) {
+				$length = $pos+1;
+				
+				if (substr_count($clause, self::STARTS_WITH, $offset, $length-$offset)) {
+					$where_vars[$var_index] = $where_vars[$var_index] . '%';
+				} else if (substr_count($clause, self::ENDS_WITH, $offset, $length-$offset)) {
+					$where_vars[$var_index] = '%' . $where_vars[$var_index];
+				} else if (substr_count($clause, self::CONTAINS, $offset, $length-$offset)) {
+					$where_vars[$var_index] = '%' . $where_vars[$var_index] . '%';
+				}
+				
+				$var_index++;
+				$offset = $length;
+			}
+
+		} while ($pos !== false);
+		
+		$clause = str_replace(self::EQUALS, 				'=', 	$clause );
+		$clause = str_replace(self::NOT_EQUALS, 			'!=', 	$clause );
+		$clause = str_replace(self::STARTS_WITH, 			'like', $clause );
+		$clause = str_replace(self::ENDS_WITH,   			'like', $clause );
+		$clause = str_replace(self::CONTAINS, 				'like', $clause );
+		$clause = str_replace(self::GREATER_THAN, 			'>', 	$clause );
+		$clause = str_replace(self::GREATER_THAN_OR_EQUAL,  '>=', 	$clause );
+		$clause = str_replace(self::LESS_THAN, 			    '<', 	$clause );
+		$clause = str_replace(self::LESS_THAN_OR_EQUAL, 	'<=', 	$clause );
+		$clause = str_replace(self::AND_THIS, 				'AND', 	$clause );
+		$clause = str_replace(self::OR_THIS, 				'OR', 	$clause );
+		
+		if ($this->relationships) {
+			$this->add_alias_to_column_names($clause);
+		}
+	}
+}
+
+?>
