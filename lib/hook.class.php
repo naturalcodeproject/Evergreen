ada0969147a4b8bb29fc3c2fd9de94b9e37a6679
wGEric <wgeric@gmail.com>
2010-07-06 21:49:28 +0000

    A really simple hooks class. Let's you add, remove and call hooks.

--- lib/hook.class.php
+++ lib/hook.class.php
@@ -30,12 +30,12 @@
 class Hook {
 	/**
 	 * stores all hooks by the location and priority they are called
-	 *
+	 * 
 	 * @access private
 	 * @static
 	 */
 	private static $hooks = array();
-
+	
 	/**
 	 * adds a hook to be executed
 	 *
@@ -44,101 +44,19 @@ class Hook {
 	 * @param string $name the location to run the hook at
 	 * @param mixed $function either a string of the function to call or an array of the class and function
 	 * @param integer $priority the order to call the function. Default is 10. Lower gets called first. Higher last.
-	 * @param string $controller
-	 * @param mixed $view
-	 * @param string $type
 	 */
-	public static function add($name, $function, $priority = 10, $controller = '', $views = array(), $type = 'all') {
+	public static function add($name, $function, $priority = 10) {
 		// make sure the function is valid
 		if (self::checkFunction($function) === false) {
 			return false;
 		}
 		
-		$views = (!is_array($views)) ? array($views) : $views;
+		// add the hook to the array
+		self::$hooks[$name][$priority][] = $function;
 		
-		$existing = &self::find($name, $function, $priority);
-
-		if (empty($existing) || sizeof($existing) > 1) {
-			// add the hook to the array
-			self::$hooks[$name][$priority][] = array(
-				'function'	=> $function,
-				'type'		=> $type,
-				'controller'	=> $controller,
-				'views'		=> (!is_array($views)) ? array($views) : $views,
-			);
-		} else {
-			$existing = &$existing[0];
-			
-			switch($type) {
-				case 'except':
-					if ($existing['type'] == 'except') {
-						foreach($views as $key => $method) {
-							if (!in_array($method, $existing['views'])) {
-								$existing['views'][] = $method;
-							}
-						}
-					} else if ($existing['type'] == 'only') {
-						$existing = array(
-							'function'	=> $function,
-							'type'		=> 'except',
-							'controller'	=> $controller,
-							'views'		=> $views,
-						);
-					}
-				break;
-				
-				case 'only':
-					if ($existing['type'] == 'except') {
-						foreach($existing['views'] as $key => $method) {
-							if (in_array($method, $views)) {
-								unset($existing['views'][$key]);
-							}
-						}
-					} else if ($existing['type'] == 'only') {
-						foreach($views as $method) {
-							if (!in_array($method, $existing['views'])) {
-								$existing['views'][] = $method;
-							}
-						}
-					}
-				break;
-				
-				default:
-					
-				break;
-			}
-		}
-
 		return true;
 	}
-
-	/**
-	 * Alias of Hook::add()
-	 *
-	 * @see Hook::add()
-	 */
-	public static function addAll($name, $function, $priority = 10, $controller = '', $view = '') {
-		return self::add($name, $function, $priority, $controller, $view, 'all');
-	}
-
-	/**
-	 * adds a hook to only one view
-	 *
-	 * @see Hook::add()
-	 */
-	public static function addOn($name, $function, $priority, $controller, $view) {
-		return self::add($name, $function, $priority, $controller, $view, 'only');
-	}
-
-	/**
-	 * adds a hook to every view except one
-	 *
-	 * @see Hook::add()
-	 */
-	public static function addExcept($name, $function, $priority, $controller, $view) {
-		return self::add($name, $function, $priority, $controller, $view, 'except');
-	}
-
+	
 	/**
 	 * removes a hook so that it isn't executed
 	 *
@@ -148,16 +66,16 @@ class Hook {
 	 * @param mixed $function either a string of the function to call or an array of the class and function
 	 * @param integer $priority the order to call the function. Default is 10. Lower gets called first. Higher last. If not specified then it will remove the hook from any priority
 	 */
-	public static function remove($name, $function = '', $priority = null, $controller = '', $view = '') {
+	public static function remove($name, $function = '', $priority = null) {
 		// if the priority is set then it is easier to remove the hook
 		if (!empty($function) && $priority !== null) {
 			if (empty(self::$hooks[$name][$priority])) {
 				return;
 			}
-
+			
 			// find the keys for the functions
 			$keys = array_keys(self::$hooks[$name][$priority], $function);
-
+			
 			// loop through and remove them
 			foreach($keys as $key) {
 				unset(self::$hooks[$name][$priority][$key]);
@@ -166,19 +84,19 @@ class Hook {
 			if (empty(self::$hooks[$name])) {
 				return;
 			}
-
+			
 			// loop through all possible hooks
 			foreach(self::$hooks[$name] as $priority => &$functions) {
 				// get the keys where the functions are
 				$keys = array_keys($functions, $function);
-
+				
 				foreach($keys as $key) {
 					unset($functions[$key]);
 				}
 			}
 		}
 	}
-
+	
 	/**
 	 * executes all hooks on a section
 	 *
@@ -189,16 +107,16 @@ class Hook {
 	 */
 	public static function call($name, $args = array()) {
 		$hooks = self::get($name);
-
+		
 		foreach($hooks as $priority => $functions) {
-			foreach($functions as $data) {
-				if (Hook::checkFunction($data['function'])) {
-					call_user_func_array($data['function'], $args);
+			foreach($functions as $function) {
+				if (Hook::checkFunction($function)) {
+					call_user_func_array($function, $args);
 				}
 			}
 		}
 	}
-
+	
 	/**
 	 * gets the hooks for location
 	 *
@@ -207,17 +125,16 @@ class Hook {
 	 * @param string $name the location of the hooks to get
 	 * @return array all of the hooks for the location
 	 */
-	public static function &get($name) {
+	public static function get($name) {
 		if (!empty(self::$hooks[$name])) {
 			asort(self::$hooks[$name]);
-
+			
 			return self::$hooks[$name];
 		} else {
-			$blank = array();
-			return $blank;
+			return array();
 		}
 	}
-
+	
 	/**
 	 * returns all of the hooks
 	 *
@@ -229,43 +146,6 @@ class Hook {
 		return self::$hooks;
 	}
 	
-	private static function &find($name, $function, $priority = null, $controller = '', $view = '') {
-		$hooks = &self::get($name);
-
-		$return = array();
-	
-		// see if we are checking a priority
-		if ($priority != null) {
-			if (empty($hooks[$priority])) {
-				$blank = array();
-				return $blank;
-			}
-			
-			// loop through each hook in the priority
-			foreach($hooks[$priority] as &$hook) {
-				// see if the function matches what we are looking for
-				if ($hook['function'] === $function) {
-					// check controller and view if they aren't empty
-					// ugly!
-					if ((!empty($controller) && !empty($view) && $hook['controller'] == $controller && in_array($view, $hook['views'])) || (!empty($controller) && $hook['controller'] == $controller) || (!empty($view) && in_array($view, $hook['views']))) {
-						$return[] = &$hook;
-					} else if (empty($controller) && empty($view)) {
-						// if they are empty but the function matches so we need to return it
-						$return[] = &$hook;
-					}
-				}
-			}
-		
-			return $return;
-		} else {
-			foreach($hooks as $priority => $hook) {
-				$return = array_merge($return, self::find($name, $function, $priority, $controller, $view));
-			}
-			
-			return $return;
-		}
-	}
-
 	/**
 	 * checks to make sure a function is valid
 	 *
@@ -277,7 +157,7 @@ class Hook {
 		if (is_array($function)) {
 			return method_exists($function[0], $function[1]);
 		}
-
+		
 		return function_exists($function);
 	}
 }
