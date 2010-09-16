c204465388eb0b018cbcaff7ec788d28f4756b76
Daniel Baldwin <dbald@stilito.com>
2010-09-15 21:51:38 -0600

    Cleaning up master to make sure that it is ready for users.

---
+++ branches/sample/controllers/main.php
@@ -0,0 +1,11 @@
+<?php
+	class Main_Sample_Controller extends ApplicationController_Sample_Controller {
+		public function __construct() {
+			// code goes here...
+		}
+	
+		public function index() {
+			// code goes here...
+		}
+	}
+?>
